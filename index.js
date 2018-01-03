83ab9bdbdfca7b4e94c229394c6de2526e29235d
Lorraine M. Gilmore <gilmore.lorraine.m@gmail.com>
2018-01-03 17:14:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+var a, b
+function add(a, b){return a+b}
+function subtract(a, b){return a-b}
+function multiply(a, b){return a*b}
+function divide(a, b){return a/b}
+function inc(n){return a+1}
+function dec(n){return a-1}
+var num=function makeInt(n)
+
+
+
+{
+  
+  
+  
+      if (n='n' {return parseInt(n)})
+   else {return "NaN"}
+}
+   
+{return parseInt(n, 10)}
+function preserveDecimal(n){return parseFloat(n)}
+
+
+ var parsed = parseInt(x, base);
+  if (isNaN(parsed)) { return 0 }
